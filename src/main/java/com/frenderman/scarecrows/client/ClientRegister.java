cb37a1ffb02cc98f633dd982fe70ab7fcfa49eb5
Sarinsa <mikkemusbruker@gmail.com>
2021-02-06 13:29:55 +0100

    Added ClientRegister.java

---
+++ src/main/java/com/frenderman/scarecrows/client/ClientRegister.java
@@ -0,0 +1,16 @@
+package com.frenderman.scarecrows.client;
+
+import com.frenderman.scarecrows.common.core.Scarecrows;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = Scarecrows.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ClientRegister {
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+
+    }
+}
