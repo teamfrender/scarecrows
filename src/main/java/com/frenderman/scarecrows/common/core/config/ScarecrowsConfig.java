8d13b4394b88a2f6cf24bc71fec02ec4cef6b59a
Sarinsa <mikkemusbruker@gmail.com>
2021-02-07 14:07:04 +0100

    Added crow spawning config option

--- src/main/java/com/frenderman/scarecrows/common/core/config/ScarecrowsConfig.java
+++ src/main/java/com/frenderman/scarecrows/common/core/config/ScarecrowsConfig.java
@@ -17,6 +17,7 @@ public class ScarecrowsConfig {
     public static class Common {
 
         private final ForgeConfigSpec.BooleanValue sadcrow;
+        private final ForgeConfigSpec.BooleanValue doCrowSpawning;
 
         public Common(ForgeConfigSpec.Builder configBuilder) {
             configBuilder.push("general");
@@ -24,11 +25,18 @@ public class ScarecrowsConfig {
             configBuilder.comment("Enables/disables the sad skin when a Scarecrow is in danger.");
             this.sadcrow = configBuilder.define("sadcrow", false);
 
+            configBuilder.comment("Enables/disables crows spawning in the world");
+            this.doCrowSpawning = configBuilder.define("doCrowSpawning", true);
+
             configBuilder.pop();
         }
 
         public boolean sadcrowEnabled() {
             return this.sadcrow.get();
+        }
+
+        public boolean crowSpawningEnabled() {
+            return this.doCrowSpawning.get();
         }
     }
 }
